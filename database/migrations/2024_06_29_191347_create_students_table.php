be530715aff24c8d3f42122c4708b5d18da5ccd0
osman707 <nazarovosman2000@gmail.com>
2024-06-30 00:54:20 +0500

    1.2.0

--- database/migrations/2024_06_29_191347_create_students_table.php
+++ database/migrations/2024_06_29_191347_create_students_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone');
+            $table->date('birthday');
+            $table->string('gender');
             $table->timestamps();
         });
     }
